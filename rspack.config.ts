cf22d0855388a062f4f7a7d4299db02b766f4d97
Tiger Kaovilai <passawit.kaovilai@gmail.com>
2026-08-07 15:44:41 -0400

    Initial commit

---
+++ rspack.config.ts
@@ -0,0 +1,98 @@
+import * as path from 'path';
+import { CopyRspackPlugin, Configuration } from '@rspack/core';
+import { ConsoleRemotePlugin } from '@openshift-console/dynamic-plugin-sdk-webpack';
+import { TsCheckerRspackPlugin } from 'ts-checker-rspack-plugin';
+
+const isProd = process.env.NODE_ENV === 'production';
+
+const config: Configuration = {
+  mode: isProd ? 'production' : 'development',
+  // No regular entry points needed. All plugin related scripts are generated via ConsoleRemotePlugin.
+  entry: {},
+  context: path.resolve(__dirname, 'src'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: isProd ? '[name]-bundle-[hash].min.js' : '[name]-bundle.js',
+    chunkFilename: isProd ? '[name]-chunk-[chunkhash].min.js' : '[name]-chunk.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(jsx?|tsx?)$/,
+        exclude: /\/node_modules\//,
+        use: {
+          loader: 'builtin:swc-loader',
+          options: {
+            detectSyntax: 'auto',
+            jsc: {
+              transform: {
+                react: {
+                  runtime: "automatic"
+                },
+                reactCompiler: {
+                  target: '18',
+                },
+              },
+              target: "es2021",
+            },
+            sourceMaps: true,
+          },
+        },
+        type: 'javascript/auto',
+      },
+      {
+        test: /\.(css)$/,
+        use: 'builtin:lightningcss-loader',
+        type: 'css',
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg|woff2?|ttf|eot|otf)(\?.*$|$)/,
+        type: 'asset/resource',
+        generator: {
+          filename: isProd ? 'assets/[contenthash][ext]' : 'assets/[name][ext]',
+        },
+      },
+      {
+        test: /\.(m?js)$/,
+        resolve: {
+          fullySpecified: false,
+        },
+      },
+    ],
+  },
+  devServer: {
+    static: './dist',
+    port: 9001,
+    // Allow Bridge running in a container to connect to the plugin dev server.
+    allowedHosts: 'all',
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      'Access-Control-Allow-Headers': 'X-Requested-With, Content-Type, Authorization',
+    },
+    devMiddleware: {
+      writeToDisk: true,
+    },
+  },
+  plugins: [
+    new ConsoleRemotePlugin(),
+    new TsCheckerRspackPlugin({
+      typescript: {
+        configFile: path.resolve(__dirname, 'tsconfig.json'),
+      },
+    }),
+    new CopyRspackPlugin({
+      patterns: [{ from: path.resolve(__dirname, 'locales'), to: 'locales' }],
+    }),
+  ],
+  devtool: isProd ? false : 'source-map',
+  optimization: {
+    chunkIds: isProd ? 'deterministic' : 'named',
+    minimize: isProd,
+  },
+};
+
+export default config;
